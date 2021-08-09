9622a8bd8106f5f6daaca09b268a7b5359a1df89
Bruno Pereira <brunorsp00@gmail.com>
2021-08-09 19:09:51 +0100

    Add new method "ReplaceAll" to extend String

---
+++ CSharpExtensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExtensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceAll(this string currentValue, List<char> oldChars, char newChar)
+        {
+            foreach (var oldChar in oldChars)
+            {
+                currentValue = currentValue.Replace(oldChar, newChar);
+            }
+            return currentValue;
+        }
+    }
+}
