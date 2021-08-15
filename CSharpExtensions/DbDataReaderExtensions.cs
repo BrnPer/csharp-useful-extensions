3ceac920ea7bf2aab0a68e32aadb7fa08c85699f
Bruno Pereira <brunorsp00@gmail.com>
2021-08-15 01:10:30 +0100

    Create DateReader extensions to be able to get the value from the reader already converted and with a default value if the column doesn't exist

---
+++ CSharpExtensions/DbDataReaderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExtensions
+{
+    public static class DbDataReaderExtensions
+    {
+
+        public static T GetValue<T>(this DbDataReader reader, string fieldName, T defaultValue)
+        {
+            //Get the field from the reader
+            var field = reader[fieldName];
+
+            //If the field is empty, return the default value
+            if (field == null) return defaultValue;
+
+            //Get the field value
+            string value = field.ToString();
+
+            dynamic returnValue;
+
+            switch (Type.GetTypeCode(typeof(T)))
+            {
+                case TypeCode.DateTime: //If type is DateTime
+
+                    DateTime temp;
+                    var couldParseDateTime = DateTime.TryParse(value, out temp);
+
+                    //If could convert, return the converted value;
+                    //Otherwise returns default value
+                    returnValue = couldParseDateTime == true ? (dynamic)temp : defaultValue;
+
+                    break;
+                case TypeCode.String:
+
+                    //Trim the value and return it
+                    value = value.TrimStart().TrimEnd();
+                    returnValue = (dynamic)value;
+                    break;
+
+
+                default: //If none of the types, return default value
+                    returnValue = defaultValue;
+                    break;
+            }
+
+            return returnValue;
+        }
+    }
+
+}
